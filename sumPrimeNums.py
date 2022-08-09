d020b70bcc788c8fa3ecd30debc0940c62ac7ba9
Kobbysaah1 <103180468+Kobbysaah1@users.noreply.github.com>
2022-08-09 14:24:45 +0200

    Update sumPrimeNums.py

--- sumPrimeNums.py
+++ sumPrimeNums.py
@@ -1,15 +1,15 @@
 def PrimeNums():
-    data= int(input("enter a number: "))
-    i=1
+    data = int (input("Enter a number: "))
     sum = 0
-    while i<=data:
-        if i+1>1 and i+1 ==3:
+    for i in range(2,data):
+        if i==2 or i==3:
+            print(i)
+            i+1
+            sum = sum+i
+        elif(i%3!=0 and i%2!=0):
+            print(i)
+            sum = sum+i
             i=i+1
-            print (i)
-        elif(i%i==0 and i%3!=0 and i%2!=0):
-            if i>=2:
-                 print (i)
-                 sum = sum+i
-        i=i+1
-    print(sum +5)
+    print(("The sum of prime numbers less than"),(data),("is"),(sum))
+
 PrimeNums()
